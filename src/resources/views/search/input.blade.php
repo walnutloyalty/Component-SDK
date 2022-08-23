6ee807a49dd8da76146aeb711f3e2934f8bad0a9
WouterLagerwerf <43637154+WouterLagerwerf@users.noreply.github.com>
2022-08-23 14:27:09 +0200

    Update input.blade.php

--- src/resources/views/search/input.blade.php
+++ src/resources/views/search/input.blade.php
@@ -1,6 +1,6 @@
 <div class="relative">
     <!-- Heroicon name: solid/search -->
-    <svg class="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    <svg style="top: 0.85rem;" class="pointer-events-none absolute left-4 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"/>
     </svg>
     <input x-ref="search" wire:keydown.enter="lookup"  wire:model="input" type="text" class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm" placeholder="Fill in your search and press enter..." role="combobox" aria-expanded="false" aria-controls="options">
