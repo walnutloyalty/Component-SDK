7e34ed0387eafc2104085d96e3331c754fc52d4c
w9009 <woutermevius@live.nl>
2022-08-31 09:28:09 +0200

    Add clickable search

--- src/Components/Search/Recent.php
+++ src/Components/Search/Recent.php
@@ -19,6 +19,22 @@ class Recent extends Component
         }
     }
 
+    public function lookup($item)
+    {
+        $this->dispatchBrowserEvent('openloader');
+        $http = Http::post(config('url.docker') . '/api/search', [
+            'search' => $item,
+            'organization_id' => auth()->user()->current_organization->id,
+        ]);
+        if(!$http->failed()) {
+            $this->emit('user', $http['user']);
+            $this->emit('results', [
+                'coupons' => $http['coupons'],
+                'products' => $http['products'],
+            ]);
+        }
+    }
+
     public function render()
     {
         return view('walletapp::search.recent');
