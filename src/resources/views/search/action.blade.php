067c1a61abe877c9a91309b47300b895e3ff6225
w9009 <woutermevius@live.nl>
2022-09-27 16:20:06 +0200

    Update action.blade.php

--- src/resources/views/search/action.blade.php
+++ src/resources/views/search/action.blade.php
@@ -19,14 +19,15 @@
                 <dt class="col-end-1 font-semibold text-gray-900">Member</dt>
                 <dd class="text-xs mt-1 truncate text-sky-600">{{ $user['identifier'] ?? 'Not provided' }}</dd>
             </dl>
-            <div class="grid grid-cols-6 gap-3">
+            <div class="grid grid-cols-6 gap-3 mt-6">
                 <a type="button"
                     @if (!auth()->user()->tokenCan('member_info')) href="#" x-data x-tooltip="You dont have access"
                 @else
                 x-data
                 x-tooltip="View profile"
+                data-tippy-arrow="false"
                 href="{{ route('member.profile.page', ['uuid' => $user['identifier'], 'tab' => 'overview']) }}" @endif
-                    class="mt-6 w-full rounded-md border border-transparent bg-sky-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none text-center focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">
+                    class="rounded-lg inline-flex p-3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 bg-sky-600 text-white hover:bg-sky-700">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
                         <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
@@ -38,8 +39,6 @@
                 x-tooltip="View passes"
                 href="{{ route('member.profile.page', ['uuid' => $user['identifier'], 'tab' => 'rewards']) }}" @endif
                     @click="type ='overview'" x-data="" x-tooltip="Overview" data-tippy-arrow="false"
-                    :class="type === 'overview' ? 'bg-sky-600 text-white hover:bg-sky-700' :
-                        'bg-sky-50 text-sky-700 hover:bg-blue-100'"
                     class="rounded-lg inline-flex p-3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 bg-sky-600 text-white hover:bg-sky-700">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path d="M4.5 3.75a3 3 0 00-3 3v.75h21v-.75a3 3 0 00-3-3h-15z" />
